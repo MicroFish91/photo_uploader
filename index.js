b6a19e0cbbddd2064fc693863764f497c45345b4
Matthew Fisher <mwfisher91@gmail.com>
2021-06-15 19:48:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const reader = new FileReader();
+const imgFile = document.querySelector('#imgFile');
+const imgPreview = document.querySelector('#imgPreview');
+const imgForm = document.querySelector('#imgForm');
+
+imgFile.addEventListener('change', function(e){
+  const selectedFile = imgFile.files[0];
+  if (selectedFile) {
+    reader.addEventListener('loadend', function(e){
+      imgPreview.src = reader.result;
+    });
+    reader.readAsDataURL(selectedFile);
+  }
+})
+
+imgForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  console.log(reader.result);
+})
